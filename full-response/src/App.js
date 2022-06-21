1c5ccae49cf1347608b4c62344c12c6ae399acf0
Ian Clark <ianclark226@gmail.com>
2022-06-21 14:58:43 -0400

    navbar and button

---
+++ full-response/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+      <Route path='/' exact />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
